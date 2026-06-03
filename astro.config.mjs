7753106da459cf81a33612969ae203e1666a3e6f
u-tv <vikkythakur1995@gmail.com>
2026-06-04 02:31:02 +0530

    Astro Dailymotion auto-sync setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+
 export default defineConfig({
   site: 'https://u-tv.github.io',
   base: '/gurubhi.github.io',
-  output: 'static'
+  output: 'static',
 });
